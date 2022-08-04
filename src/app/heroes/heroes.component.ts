1e8184a15b7ff298e7ba492f22d5e956faeb4dfa
SelinaRAA <110453890+SelinaRAA@users.noreply.github.com>
2022-08-04 15:22:38 +0100

    Initial commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from 'src/app/hero';
+import { HEROES } from 'src/app/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+
+})
+export class HeroesComponent implements OnInit {
+
+  heros = HEROES;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+}
+

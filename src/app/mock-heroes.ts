a2b9184083e5275e52a5fadcc3d6be45c6b27405
SelinaRAA <110453890+SelinaRAA@users.noreply.github.com>
2022-08-10 16:41:03 +0100

    Added bcakground colour to buttons

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 //is line 3-13 mocked data that will replace the id and name in the heros.component.ts file?
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr. IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
+  { id: 12, name: 'Dr. Nice', colour: 'Black' },
+  { id: 13, name: 'Bombasto', colour: 'White' },
+  { id: 14, name: 'Celeritas', colour: 'Purple' },
+  { id: 15, name: 'Magneta', colour: 'Blue' },
+  { id: 16, name: 'RubberMan', colour: 'Orange' },
+  { id: 17, name: 'Dynama', colour: 'Yellow' },
+  { id: 18, name: 'Dr. IQ', colour: 'Red' },
+  { id: 19, name: 'Magma', colour: 'Green' },
+  { id: 20, name: 'Tornado', colour: 'Pink' }
 ];

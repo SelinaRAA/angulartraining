8b59de4aec1f34416cd42e4b3d4b26440518e6b5
SelinaRAA <110453890+SelinaRAA@users.noreply.github.com>
2022-08-18 15:44:52 +0100

    Session 4 homework

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,13 +1,13 @@
 import { Hero } from './hero';
 //is line 3-13 mocked data that will replace the id and name in the heros.component.ts file?
 export const HEROES: Hero[] = [
-  { id: 12, name: 'Dr. Nice', colour: 'Black' },
-  { id: 13, name: 'Bombasto', colour: 'White' },
-  { id: 14, name: 'Celeritas', colour: 'Purple' },
-  { id: 15, name: 'Magneta', colour: 'Blue' },
-  { id: 16, name: 'RubberMan', colour: 'Orange' },
-  { id: 17, name: 'Dynama', colour: 'Yellow' },
-  { id: 18, name: 'Dr. IQ', colour: 'Red' },
-  { id: 19, name: 'Magma', colour: 'Green' },
-  { id: 20, name: 'Tornado', colour: 'Pink' }
+  { id: 12, name: 'Dr. Nice', colour: 'Black', power: 'Teleport' },
+  { id: 13, name: 'Bombasto', colour: 'White', power: 'Fly' },
+  { id: 14, name: 'Celeritas', colour: 'Purple', power: 'levitate' },
+  { id: 15, name: 'Magneta', colour: 'Blue', power: 'Time travel' },
+  { id: 16, name: 'RubberMan', colour: 'Orange', power: 'Rain' },
+  { id: 17, name: 'Dynama', colour: 'Yellow', power: 'Pause time' },
+  { id: 18, name: 'Dr. IQ', colour: 'Red', power: 'See into the future' },
+  { id: 19, name: 'Magma', colour: 'Green', power: 'Shapeshifting' },
+  { id: 20, name: 'Tornado', colour: 'Pink', power: 'Magic' }
 ];
